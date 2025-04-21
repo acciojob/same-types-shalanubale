c73b055cf417b1e557986252aa26f8280789c3f4
shalanubale <ubaleshalan1836@gmail.com>
2025-04-22 01:39:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function isSameType(value1, value2) {
   //your js code here
+	console.log(isSameType(1, 3));        // true
+console.log(isSameType("hey", "hi")); // true
+console.log(isSameType(NaN, NaN));    // true
+console.log(isSameType("3", 3));      // false
+console.log(isSameType(undefined, null)); // false
+console.log(isSameType({}, []));      // false (both are objects, but different kinds)
+
 }
 
 // do not change the code below.
